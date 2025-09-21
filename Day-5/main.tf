6221c0fe3d45e853f7ad12decd203cdd9a8e5b00
devopswithdj <bdhanunjayardy@gmail.com>
2025-09-21 14:44:00 +0530

    workspaces with builtin lookup function to pick value

--- Day-5/main.tf
+++ Day-5/main.tf
@@ -14,15 +14,19 @@ provider "aws" {
 
 variable "instance_type" {
     description = "Type of instance to create"
-    type        = string
-    default     = "t2.micro"
+    type        = map(string)
+    default     = {
+        "dev"= "t2.micro"
+        "stage" = "t2.medium"
+        "prod" = "t2.large"
+    }
 }
 
 
 
 resource "aws_instance" "example" {
     ami           = "ami-06a974f9b8a97ecf2" # Amazon Linux 3 AMI (HVM), SSD Volume Type
-    instance_type = var.instance_type
+    instance_type = lookup(var.instance_type, terraform.workspace, "t3.micro")
 
     tags = {
         Name = "tf-learning-instance"
